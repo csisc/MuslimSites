fe2665e53c5a4af4e10ce8fc53b218b610d7779f
Houcemeddine Turki <turkiabdelwaheb@hotmail.fr>
2024-04-03 18:13:48 +0100

    Update leaflet.groupedlayercontrol.js

--- assets/leaflet-groupedlayercontrol/leaflet.groupedlayercontrol.js
+++ assets/leaflet-groupedlayercontrol/leaflet.groupedlayercontrol.js
@@ -289,3 +289,6 @@ L.Control.GroupedLayers = L.Control.extend({
 L.control.groupedLayers = function (baseLayers, groupedOverlays, options) {
   return new L.Control.GroupedLayers(baseLayers, groupedOverlays, options);
 };
+
+//Check mosques by default
+document.querySelector("input.leaflet-control-layers-selector[type='checkbox']").click()
